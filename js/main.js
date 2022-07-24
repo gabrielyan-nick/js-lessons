543d22567979fcb1312f4d1e37e139fdf82b782a
Nick <junk72305@gmail.com>
2022-07-24 16:29:05 +0300

    my first js app

---
+++ js/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// alert('wtf');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('How old are you?', '');
+// console.log(answer + 10);
+
+// const answers = [];
+
+// // answers[0] = prompt('What is your name?', '');
+// // answers[1] = prompt('What is your surname?', '');
+// // answers[2] = prompt('How old are you?', '');
+
+// console.log(typeof(answers));
+
+
+// const user = 'Nick';
+// alert(`Hello, ${user}`);
+
+// let incr = 20,
+//     decr = 30;
+
+//     incr++;
+//     decr--;
+
+//     console.log(decr);
+//     console.log(incr);
+    
+console.log(10%4);
+
+console.log(6*4 == 24);
+
+
+const isChecked = true,
+      isClose = true,
+      isFuck = true;
+
+      console.log(isChecked && isClose && isFuck);
