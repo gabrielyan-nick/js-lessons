71a4df8f56cf38a5674bd179000b75aa35c45b82
Nick <junk72305@gmail.com>
2022-07-27 13:26:16 +0300

    commit

--- js.js
+++ js.js
@@ -1,24 +1,51 @@
 "use strict";
 
-const checkBox = document.querySelector("input");
-const para = document.querySelector("p");
-let shoppingDone = false;
+// const checkBox = document.querySelector("input");
+// const para = document.querySelector("p");
+// let shoppingDone = false;
 
-checkBox.addEventListener("change", () => {
-  checkBox.disabled = true;
-  shoppingDone = true;
-  updateAllowance();
-});
+// checkBox.addEventListener("change", () => {
+//   checkBox.disabled = true;
+//   shoppingDone = true;
+//   updateAllowance();
+// });
 
-function updateAllowance() {
-  let childsAllowance;
-  if (shoppingDone === true) {
-    childsAllowance = 10;
-  } else {
-    childsAllowance = 5;
-  }
+// function updateAllowance() {
+//   let childsAllowance;
+//   if (shoppingDone === true) {
+//     childsAllowance = 10;
+//   } else {
+//     childsAllowance = 5;
+//   }
 
-  para.textContent = `Child has earned \$${childsAllowance} this week.`;
+//   para.textContent = `Child has earned \$${childsAllowance} this week.`;
+// }
+
+// updateAllowance();
+
+
+const select = document.querySelector('select');
+const para = document.querySelector('p');
+
+select.onchange = setWeather;
+
+function setWeather() {
+    const choise = select.value;
+
+    switch (choise) {
+    case 'sunny': 
+        para.textContent = "It's sunny today. Go to the beatch and take ice cream.";
+        break;
+    case 'rainy': 
+        para.textContent = 'Rainy outside the window. Take an ambrella or stay home.';
+        break;
+    case 'snowing': 
+        para.textContent = 'Snowing and cold. Make a cup of hot tea and relax.';
+        break;
+    case 'overcast':
+        para.textContent = 'A lot of clouds in the sky. Make sure that you take an amrella.';
+        break;
+        default:
+        para.textContent = '';
+    }
 }
-
-updateAllowance();
--- js/main.js
+++ js/main.js
@@ -39,40 +39,117 @@
 //       isFuck = true;
 
 //       console.log(isChecked && isClose && isFuck);
-if (4 == 9) {
-    console.log('Ok!');
-} else {
-    console.log('error');
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('error');
+// }
+
+// const num = 47;
+
+// if (num > 48) {
+//     console.log('error');
+// } else if (num < 46) {
+//     console.log('error');
+// } else if (num !== 46) {
+//     console.log('Nooo');
+// } else {
+//     console.log('OK');
+// }
+
+// (num !== 47) ? console.log('ok') : console.log('error');
+
+
+// const abc = 69;
+
+// switch (abc) {
+//     case 68:
+//         console.log('No');
+//         break;
+//     case 70:
+//         console.log('No');
+//         break;
+//     case 64:
+//         console.log('Yes');
+//         break;
+//     default:
+//         console.log('Try again');
+//         break;
+//     }
+
+
+//     var cheese = 'Cheaddar';
+//     var cheese = false;
+
+//     if (cheese) {
+//         console.log('Yes!');
+//     } else {
+//         console.log('No!');
+//     }
+
+
+    // const burger = 3;
+    // const fries = 1;
+    // const cola = 0;
+
+    // console.log(burger === 3 && cola && fries);
+
+    // if (burger ===3 && cola && fries) {
+    //     console.log('yes');
+    // } else {
+    //     console.log('no');
+    // }
+
+
+    // console.log( NaN || 2 || undefined );
+    // console.log( NaN && 2 && undefined );
+    // console.log( 1 && 2 && 3 );
+    // console.log( !1 && 2 || !3);
+    // console.log( 25 || null && !3 );
+    // console.log( NaN || null || !3 || undefined || 5);
+    // console.log( NaN || null && !3 && undefined || 5);
+    // console.log( 5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// }
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 1;
+ 
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// } else {
+//     console.log('No');
+// }
+
+let num = 50;
+
+while (num <= 60) {
+    console.log(num);
+    num++;
 }
 
-const num = 47;
 
-if (num > 48) {
-    console.log('error');
-} else if (num < 46) {
-    console.log('error');
-} else if (num !== 46) {
-    console.log('Nooo');
-} else {
-    console.log('OK');
-}
-
-(num !== 47) ? console.log('ok') : console.log('error');
-
-
-const abc = 69;
-
-switch (abc) {
-    case 68:
-        console.log('No');
-        break;
-    case 70:
-        console.log('No');
-        break;
-    case 64:
-        console.log('Yes');
-        break;
-    default:
-        console.log('Try again');
-        break;
+for (let i = 1; i <= 100; i++ ) {
+    if (i % 17 == 0) {
+        console.log( i );
     }
+}
