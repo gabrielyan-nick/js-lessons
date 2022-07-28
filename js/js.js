59b300cae1de55c6253692fe1252ff79078cd902
Nick <junk72305@gmail.com>
2022-07-28 16:47:33 +0300

    commit

--- js/js.js
+++ js/js.js
@@ -1,5 +1,255 @@
 "use strict";
 
+// let incr = 20,
+//     decr = 30;
+
+//     incr++;
+//     decr--;
+
+//     console.log(decr);
+//     console.log(incr);
+
+// console.log(10%4);
+
+// console.log(6*4 == 24);
+
+// const isChecked = true,
+//       isClose = true,
+//       isFuck = true;
+
+//       console.log(isChecked && isClose && isFuck);
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('error');
+// }
+
+// const num = 47;
+
+// if (num > 48) {
+//     console.log('error');
+// } else if (num < 46) {
+//     console.log('error');
+// } else if (num !== 46) {
+//     console.log('Nooo');
+// } else {
+//     console.log('OK');
+// }
+
+// (num !== 47) ? console.log('ok') : console.log('error');
+
+// const abc = 69;
+
+// switch (abc) {
+//     case 68:
+//         console.log('No');
+//         break;
+//     case 70:
+//         console.log('No');
+//         break;
+//     case 64:
+//         console.log('Yes');
+//         break;
+//     default:
+//         console.log('Try again');
+//         break;
+//     }
+
+//     var cheese = 'Cheaddar';
+//     var cheese = false;
+
+//     if (cheese) {
+//         console.log('Yes!');
+//     } else {
+//         console.log('No!');
+//     }
+
+// const burger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(burger === 3 && cola && fries);
+
+// if (burger ===3 && cola && fries) {
+//     console.log('yes');
+// } else {
+//     console.log('no');
+// }
+
+// console.log( NaN || 2 || undefined );
+// console.log( NaN && 2 && undefined );
+// console.log( 1 && 2 && 3 );
+// console.log( !1 && 2 || !3);
+// console.log( 25 || null && !3 );
+// console.log( NaN || null || !3 || undefined || 5);
+// console.log( NaN || null && !3 && undefined || 5);
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 1;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// } else {
+//     console.log('No');
+// }
+
+// let num = 50;
+
+// while (num <= 60) {
+//   console.log(num);
+//   num++;
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// let i = 0;
+// while (i < 3) {
+//   alert(`number ${i}!`);
+//   i++;
+// }
+
+// let result = "";
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+//   for (let j = 0; j < i; j++) {
+//     result += "*";
+//   }
+//   result += "\n";
+// }
+// console.log(result);
+// return result;
+
+// first: for (let i = 0; i < 3; i++) {
+//   console.log(`first level: ${i}`);
+//   for (let j = 0; j < 3; j++) {
+//     console.log(`second level: ${j}`);
+//     for (let k = 0; k < 3; k++) {
+//         if (k === 2) break first;
+//       console.log(`third level: ${k}`);
+//     }
+//   }
+// }
+
+// for (i = 5; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for (i = 20; i > 10; i--) {
+//   if (i == 13) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// for (i = 2; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// let j = 2;
+
+// while (j < 16) {
+//   j++;
+//   if (j % 2 === 0) {
+//     continue;
+//   } else {
+//     console.log(j);
+//   }
+// }
+
+// const arrayOfNumbers = [];
+
+// for (k = 5; k < 11; k++) {
+//   arrayOfNumbers[k - 5] = k;
+// }
+// console.log(arrayOfNumbers);
+// return arrayOfNumbers;
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//   result[i] = arr[i];
+// }
+// console.log(result);
+// return result;
+
+// const data = [5, 10, "Shopping", 20, "Homework"];
+
+// for (let i = 0; i < data.length; i++) {
+//   if (typeof data[i] === "number") {
+//     data[i] = data[i] * 2;
+//   } else if (typeof data[i] === "string") {
+//     data[i] = `${data[i]} - done`;
+//   }
+// }
+// console.log(data);
+
+// const data = [5, 10, "Shopping", 20, "Homework"];
+// const result = [];
+
+// for (let i = 1; i <= data.length; i++) {
+//     result[i - 1] = data[data.length - i]
+// }
+// console.log(result);
+// return result;
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += '';
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += '*';
+//     }
+//     result += '\n';
+// }
+// console.log(result);
+
+// let admin;
+// let name;
+
+// name = 'John';
+// name = admin;
+// alert(admin);
+
 // const checkBox = document.querySelector("input");
 // const para = document.querySelector("p");
 // let shoppingDone = false;
@@ -23,7 +273,6 @@
 
 // updateAllowance();
 
-
 // const select = document.querySelector('select');
 // const para = document.querySelector('p');
 
@@ -33,13 +282,13 @@
 //     const choise = select.value;
 
 //     switch (choise) {
-//     case 'sunny': 
+//     case 'sunny':
 //         para.textContent = "It's sunny today. Go to the beatch and take ice cream.";
 //         break;
-//     case 'rainy': 
+//     case 'rainy':
 //         para.textContent = 'Rainy outside the window. Take an ambrella or stay home.';
 //         break;
-//     case 'snowing': 
+//     case 'snowing':
 //         para.textContent = 'Snowing and cold. Make a cup of hot tea and relax.';
 //         break;
 //     case 'overcast':
@@ -50,7 +299,94 @@
 //     }
 // }
 
-
 // let nick = prompt('What is your name?', '');
 // alert(`you are ${nick}.`);
+// let num = 20;
 
+// function showFirstMessege(text) {
+//   console.log(text);
+//   console.log(num);
+// }
+// showFirstMessege("Hello");
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a * b);
+// }
+// console.log(calc(4, 3));
+
+// function ret() {
+//     let abc = 33;
+//     return abc;
+// }
+
+// const dfg = ret();
+// console.log(dfg);
+
+// const logger = function () {
+//     console.log('Ok');
+// };
+// logger();
+
+// const calc = (a, b, c) => {
+//   return a * b + c;
+// };
+// console.log(calc(1, 2, 3));
+
+// function calc(a, b) {
+//   return a * b;
+// }
+// console.log(calc(2, 3));
+
+// let a = 3;
+// function addTwo(x) {
+//   let ret = x + 2;
+//   return ret;
+// }
+// let b = addTwo(a);
+// console.log(b);
+
+// let val1 = 2;
+// function multiplyThis(n) {
+//   let ret = n * val1;
+//   return ret;
+// }
+// let multiplied = multiplyThis(6);
+// console.log("example of scope:", multiplied);
+
+// let val = 7;
+// function createAdder() {
+//   function addNumbers(a, b) {
+//     let ret = a + b;
+//     return ret;
+//   }
+//   return addNumbers;
+// }
+// let adder = createAdder();
+// let sum = adder(val, 8);
+// console.log("example of function returning a function: ", sum);
+
+// function createCounter() {
+//   let counter = 0;
+//   const myFunction = function () {
+//     counter = counter + 1;
+//     return counter;
+//   };
+//   return myFunction;
+// }
+// const increment = createCounter();
+// const c1 = increment();
+// const c2 = increment();
+// const c3 = increment();
+// console.log("example increment", c1, c2, c3);
+
+
+// let c = 4
+// function addX(x) {
+//   return function(n) {
+//      return n + x
+//   }
+// }
+// const addThree = addX(3)
+// let d = addThree(c)
+// console.log('example partial application', d)
--- js/main.js
+++ js/main.js
@@ -1,272 +1,66 @@
 "use strict";
 
-// alert('wtf');
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
-// const result = confirm('Are you here?');
-// console.log(result);
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
 
-// const answer = +prompt('How old are you?', '');
-// console.log(answer + 10);
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done');
+        } else {
+            console.log('Error');
+            i--;
+        }
+}
 
-// const answers = [];
-
-// // answers[0] = prompt('What is your name?', '');
-// // answers[1] = prompt('What is your surname?', '');
-// // answers[2] = prompt('How old are you?', '');
-
-// console.log(typeof(answers));
-
-// const user = 'Nick';
-// alert(`Hello, ${user}`);
-
-// let incr = 20,
-//     decr = 30;
-
-//     incr++;
-//     decr--;
-
-//     console.log(decr);
-//     console.log(incr);
-
-// console.log(10%4);
-
-// console.log(6*4 == 24);
-
-// const isChecked = true,
-//       isClose = true,
-//       isFuck = true;
-
-//       console.log(isChecked && isClose && isFuck);
-// if (4 == 9) {
-//     console.log('Ok!');
-// } else {
-//     console.log('error');
-// }
-
-// const num = 47;
-
-// if (num > 48) {
-//     console.log('error');
-// } else if (num < 46) {
-//     console.log('error');
-// } else if (num !== 46) {
-//     console.log('Nooo');
-// } else {
-//     console.log('OK');
-// }
-
-// (num !== 47) ? console.log('ok') : console.log('error');
-
-// const abc = 69;
-
-// switch (abc) {
-//     case 68:
-//         console.log('No');
-//         break;
-//     case 70:
-//         console.log('No');
-//         break;
-//     case 64:
-//         console.log('Yes');
-//         break;
-//     default:
-//         console.log('Try again');
-//         break;
-//     }
-
-//     var cheese = 'Cheaddar';
-//     var cheese = false;
-
-//     if (cheese) {
-//         console.log('Yes!');
-//     } else {
-//         console.log('No!');
-//     }
-
-// const burger = 3;
-// const fries = 1;
-// const cola = 0;
-
-// console.log(burger === 3 && cola && fries);
-
-// if (burger ===3 && cola && fries) {
-//     console.log('yes');
-// } else {
-//     console.log('no');
-// }
-
-// console.log( NaN || 2 || undefined );
-// console.log( NaN && 2 && undefined );
-// console.log( 1 && 2 && 3 );
-// console.log( !1 && 2 || !3);
-// console.log( 25 || null && !3 );
-// console.log( NaN || null || !3 || undefined || 5);
-// console.log( NaN || null && !3 && undefined || 5);
-// console.log( 5 === 5 && 3 > 1 || 5);
-
-// const hamburger = 3;
-// const fries = 3;
-// const cola = 0;
-// const nuggets = 2;
-
-// if (hamburger === 3 && cola || fries === 3 && nuggets) {
-//    console.log('Done!');
-// }
-
-// let hamburger;
-// const fries = NaN;
-// const cola = 0;
-// const nuggets = 1;
-
-// if (hamburger || cola || fries === 3 || nuggets) {
-//    console.log('Done!');
-// }
-
-// let hamburger;
-// const fries = NaN;
-// const cola = 0;
-// const nuggets = 2;
-
-// if (hamburger && cola || fries === 3 && nuggets) {
-//    console.log('Done!');
-// } else {
-//     console.log('No');
-// }
-
-// let num = 50;
-
-// while (num <= 60) {
-//   console.log(num);
-//   num++;
-// }
-
-// for (let i = 1; i <= 10; i++) {
-//   if (i % 2 !== 0) {
-//     console.log(i);
+// let i = 0;
+// while (i < 2) {
+//   const a = prompt("Один из последних просмотренных фильмов?", ""),
+//     b = prompt("На сколько оцените его?", "");
+//   i++;
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log("Done");
+//   } else {
+//     console.log("Error");
+//     i--;
 //   }
 // }
 
 // let i = 0;
-// while (i < 3) {
-//   alert(`number ${i}!`);
-//   i++;
-// }
+// do {
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//          b = prompt("На сколько оцените его?", "");
+//        i++;
+//        if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//              personalMovieDB.movies[a] = b;
+//              console.log("Done");
+//            } else {
+//              console.log("Error");
+//              i--;
+//            }
+// } while (i < 2);
 
-// let result = "";
-// const length = 7;
 
-// for (let i = 1; i < length; i++) {
-//   for (let j = 0; j < i; j++) {
-//     result += "*";
-//   }
-//   result += "\n";
-// }
-// console.log(result);
-// return result;
 
-// first: for (let i = 0; i < 3; i++) {
-//   console.log(`first level: ${i}`);
-//   for (let j = 0; j < 3; j++) {
-//     console.log(`second level: ${j}`);
-//     for (let k = 0; k < 3; k++) {
-//         if (k === 2) break first;
-//       console.log(`third level: ${k}`);
-//     }
-//   }
-// }
-
-// for (i = 5; i <= 10; i++) {
-//   console.log(i);
-// }
-
-// for (i = 20; i > 10; i--) {
-//   if (i == 13) {
-//     break;
-//   }
-//   console.log(i);
-// }
-
-// for (i = 2; i <= 10; i++) {
-//   if (i % 2 == 0) {
-//     console.log(i);
-//   }
-// }
-
-// for (let i = 2; i <= 16; i++) {
-//   if (i % 2 === 0) {
-//     continue;
-//   } else {
-//     console.log(i);
-//   }
-// }
-
-// let j = 2;
-
-// while (j < 16) {
-//   j++;
-//   if (j % 2 === 0) {
-//     continue;
-//   } else {
-//     console.log(j);
-//   }
-// }
-
-// const arrayOfNumbers = [];
-
-// for (k = 5; k < 11; k++) {
-//   arrayOfNumbers[k - 5] = k;
-// }
-// console.log(arrayOfNumbers);
-// return arrayOfNumbers;
-
-// const arr = [3, 5, 8, 16, 20, 23, 50];
-// const result = [];
-
-// for (let i = 0; i < arr.length; i++) {
-//   result[i] = arr[i];
-// }
-// console.log(result);
-// return result;
-
-// const data = [5, 10, "Shopping", 20, "Homework"];
-
-// for (let i = 0; i < data.length; i++) {
-//   if (typeof data[i] === "number") {
-//     data[i] = data[i] * 2;
-//   } else if (typeof data[i] === "string") {
-//     data[i] = `${data[i]} - done`;
-//   }
-// }
-// console.log(data);
-
-// const data = [5, 10, "Shopping", 20, "Homework"];
-// const result = [];
-
-// for (let i = 1; i <= data.length; i++) {
-//     result[i - 1] = data[data.length - i]
-// }
-// console.log(result);
-// return result;
-
-const lines = 5;
-let result = '';
-
-for (let i = 0; i <= lines; i++) {
-    for (let j = 0; j < lines - i; j++) {
-        result += '';
-    }
-    for (let j = 0; j < 2 * i + 1; j++) {
-        result += '*';
-    }
-    result += '\n';
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
 }
-console.log(result);
 
+console.log(personalMovieDB);
 
-
-let admin;
-let name;
-
-name = 'John';
-name = admin;
-alert(admin);
