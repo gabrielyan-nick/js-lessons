938d93daad16a50cd1ff7f7af6cee891d5bcf22a
Nick <junk72305@gmail.com>
2022-08-05 00:00:05 +0300

    commit

--- js/js.js
+++ js/js.js
@@ -1009,3 +1009,40 @@
 //     }
 // }
 // console.log(isBudgetEnough(shoppingMallData));
+
+// const students = [
+//   "Peter",
+//   "Andrew",
+//   "Ann",
+//   "Mark",
+//   "Josh",
+//   "Sandra",
+//   "Cris",
+//   "Bernard",
+//   "Takesi",
+//   "Sam",
+//   'SUNV',
+//   'bdt'
+// ];
+
+// function sortStudentsByGroups(arr) {
+//   arr.sort();
+//   let a = [],
+//     b = [],
+//     c = [],
+//     rest = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i < 3) {
+//             a.push(arr[i]);
+//         } else if (i < 6) {
+//             b.push(arr[i]);
+//         } else if (i < 9) {
+//             c.push(arr[i]);
+//         } else {
+//             rest.push(arr[i]);
+//         }
+//     }
+//     return [a, b, c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`];
+// }
+// console.log(sortStudentsByGroups(students));
---
+++ js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// function hello() {
+//     console.log('Hello world');
+// }
+// hello();
+
+// function hi() {
+//     console.log('Hi');
+// }
+// hi();
+
+// const arr = [1, 14, 4, 30, 54],
+//     sorted = arr.sort(compareNum);
+
+//     function compareNum(a, b) {
+//         return a - b;
+//     }
+
+// console.log(sorted);
+
+let number = 5; debugger
+
+function logNumber () {
+    let number = 4; debugger
+    console.log(number);
+}
+
+number = 6;
+
+logNumber(); debugger
